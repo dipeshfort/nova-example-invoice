70e6e6e2e21f05e68fa7b08188a5e2e8ddd5390d
dipesh <dipesh.fort@gmail.com>
2018-12-09 23:56:25 +0200

    Added docker start npm script

--- utils/logger.js
+++ utils/logger.js
@@ -2,7 +2,7 @@ var bunyan = require('bunyan');
 
 const logger = bunyan.createLogger({
     name: `${process.env.APPLICATION_NAME}:${process.env.APPLICATION_VERSION}`,
-    level: 'trace'
+    level: 'debug'
 });
 
 module.exports = logger;
